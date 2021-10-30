07be8bcea41d22197a967f5dbb759953b8e48686
Dimitris <dimitriskl@debian-BULLSEYE-live-builder-AMD64>
2021-10-31 02:01:27 +0300

    explanations

--- src/main.rs
+++ src/main.rs
@@ -21,6 +21,14 @@ async fn main() {
 		let tx = tx.clone();
 		let mut rx = tx.subscribe();
 
+		/*
+		** We are about to spawn a task. A bit of wisdom then!
+		** When we have a finite amount of things that need to operate
+		** in a shared state, we prefer select, as we will see later.
+		** In a case like this where we can expect pretty much any amount
+		** of incoming connections, we will spawn tasks.
+		*/
+
 		// Spawn a task for this socket
 		tokio::spawn(async move {
 
@@ -38,7 +46,14 @@ async fn main() {
 				** interfere with eachother's read/write) we use tokio's select
 				** macro. Basically we an outcome depending on what happens.
 				**
+				** In this case we indeed have finite things plus they are dependent
+				** on the same source in the way that we splitted the socket to get
+				** sender and writer.
+				**
 				** Note that these are Futures and thus have been already awaited.
+				**
+				** Also, not that select blocks when a result is selected so the
+				** two can't mess with eachother.
 				*/
 				tokio::select! {
 					result = reader.read_line(&mut line) => {
@@ -46,12 +61,17 @@ async fn main() {
 						if result.unwrap() == 0 {
 							break;
 						}
-						tx.send(line.clone()).unwrap();
+
+						
+						tx.send((line.clone(), addr)).unwrap();
 						line.clear();
 					}
 					result = rx.recv() => {
-						let msg = result.unwrap();
-						writer.write_all(msg.as_bytes()).await.unwrap();						
+						let (msg, other_addr) = result.unwrap();
+
+						if addr != other_addr {
+							writer.write_all(msg.as_bytes()).await.unwrap();
+						}
 					}
 				}
 			}
